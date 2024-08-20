f4ca69b8c2b1f4e97c96578c52248510fa63bec6
PravallikaDeepu <medurimoses@gmail.com>
2024-08-20 18:02:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [name,setName] = useState("")
+  const [age,setAge] = useState("")
+
+  function collectName(e)
+  {
+    setName(e.target.value)
+  }
+
+  function collectAge(e)
+  {
+    setAge(e.target.value)
+  }
+  function validatedetails(e)
+  {
+    e.preventDefault()
+    setAge("")
+    setName("")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{marginTop: "50px"}}>
+
+     <form method="POST" onSubmit={validatedetails}>
+      <label>Name:</label>
+      <input type="text" onChange={collectName} value={name} /><br/>
+      <label>Age:</label>
+      <input type="number" onChange={collectAge} value={age}/><br/>
+    
+
+     <div>
+      {name && age ? <button>Submit</button> : <h4>Please fill the required details</h4>}
+     </div>
+     </form>
     </div>
   );
 }
